e779821aa62eb61a731927c8fd7ae4d973e577b4
anannyashreesharma <ananyashreesharma19@gmail.com>
2026-03-30 08:30:07 +0530

    updates

--- portfolio-react/src/App.tsx
+++ portfolio-react/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PurrfolioHero } from './components/PurrfolioHero';
 import { NotFoundPage } from './components/NotFoundPage';
 import { EditorialIdleGame } from './components/EditorialIdleGame';
 import { WorkBossPage } from './components/WorkBossPage';
@@ -10,7 +11,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={ROUTES.root} element={<Navigate to={ROUTES.editorial} replace />} />
+        <Route path={ROUTES.root} element={<PurrfolioHero />} />
         <Route path={ROUTES.editorial} element={<EditorialIdleGame />} />
         <Route path={ROUTES.workBoss} element={<WorkBossPage />} />
         <Route path={ROUTES.projectsBoss} element={<ProjectsBossPage />} />
